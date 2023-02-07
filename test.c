7cd0bfcab6031eabdb6a591e787f08fb3895d004
Uttam <utam.shrestha65@gmail.com>
2023-02-07 20:47:17 +0545

    Latest update

--- test.c
+++ test.c
@@ -3,16 +3,25 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 int tableno;
 void ask_prompt();
 void username_prompt();
 void password_prompt();
 void tableNumber();
 void reviews();
+void menus();
+void clearscreen();
+
+void clearscreen()
+{
+    system("cls");
+}
 int main()
 {
+    menus();
     // tableNumber();
-    reviews();
+    // reviews();
     // username_prompt();
     // ask_prompt();
     return 0;
@@ -28,38 +37,66 @@ void tableNumber()
 void reviews()
 {
     int excellent, good, average, dissatisfied, first, second, third, fourth, fifth, sixth, seventh;
-    char words[50];
+    char words[500];
     printf("Food Quality: ");
     printf("\n1. Excellent \n2. Good \n3. Average \n4. Dissatisfied\n");
     scanf("%d", &first);
     printf("Your answer is %d.\n", first);
+    clearscreen();
     printf("Overall Service Quality: ");
     printf("\n1. Excellent \n2. Good \n3. Average \n4. Dissatisfied\n");
     scanf("%d", &second);
     printf("Your answer is %d.\n", second);
+    clearscreen();
     printf("Cleanliness: ");
     printf("\n1. Excellent \n2. Good \n3. Average \n4. Dissatisfied\n");
     scanf("%d", &third);
     printf("Your answer is %d.\n", third);
+    clearscreen();
     printf("Order Accuracy: ");
     printf("\n1. Excellent \n2. Good \n3. Average \n4. Dissatisfied\n");
     scanf("%d", &fourth);
     printf("Your answer is %d.\n", fourth);
+    clearscreen();
     printf("Speed of Service: ");
     printf("\n1. Excellent \n2. Good \n3. Average \n4. Dissatisfied\n");
     scanf("%d", &fifth);
     printf("Your answer is %d.\n", fifth);
+    clearscreen();
     printf("Value: ");
     printf("\n1. Excellent \n2. Good \n3. Average \n4. Dissatisfied\n");
     scanf("%d", &sixth);
     printf("Your answer is %d.\n", sixth);
+    clearscreen();
     printf("Overall Experience: ");
     printf("\n1. Excellent \n2. Good \n3. Average \n4. Dissatisfied\n");
     scanf("%d", &seventh);
     printf("Your answer is %d.\n", seventh);
+    clearscreen();
+
     printf("Any comments, questions or suggestions?\n");
-    scanf("%[^/n]", words);
+    scanf("%s", &words);
     printf("Your review: %s", words);
+    clearscreen();
+    printf("Thank you for your response");
+}
+
+void menus()
+{
+    FILE *fp;
+    char s[2000];
+    fp = fopen("menus.txt", "r");
+    if (fp == NULL)
+    {
+        printf("Can't open menu right now.");
+    }
+    else
+    {
+        printf("\n Your menu is: \n");
+    }
+    fgets(s, 2000, fp);
+    printf("\n Your menu: %s \n", s);
+    fclose(fp);
 }
 
 void username_prompt()
